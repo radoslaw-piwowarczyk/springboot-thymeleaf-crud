c3299411a81864bc9709925e60be999cecc55e7f
Radoslaw Piwowarczyk <rad.piwowarczyk@gmail.com>
2021-01-05 11:20:35 +0100

    finished add employee feature

--- src/main/java/com/example/springbootthymeleafcrudwebapp/controller/EmployeeController.java
+++ src/main/java/com/example/springbootthymeleafcrudwebapp/controller/EmployeeController.java
@@ -1,10 +1,13 @@
 package com.example.springbootthymeleafcrudwebapp.controller;
 
+import com.example.springbootthymeleafcrudwebapp.model.Employee;
 import com.example.springbootthymeleafcrudwebapp.service.EmployeeService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @AllArgsConstructor
@@ -12,9 +15,24 @@ public class EmployeeController {
 
     private EmployeeService employeeService;
 
+    //display list of employees
     @GetMapping("/")
     public String viewHomePage(Model model) {
         model.addAttribute("listEmployees", employeeService.getAllEmployees());
         return "index";
+    }
+
+    //create mode attribute to bind form data
+    @GetMapping("/showNewEmployeeForm")
+    public String showNewEmployeeForm(Model model) {
+        Employee employee = new Employee();
+        model.addAttribute("employee", employee);
+        return "new_employee";
+    }
+    @PostMapping("/saveEmployee")
+    public String saveEmployee(@ModelAttribute("employee") Employee employee){
+        //save employee to database
+        employeeService.saveEmployee(employee);
+        return "redirect:/";
     }
 }
