bbc0469d597b11bf19ddd6c1ee62a2b7656670aa
Radoslaw Piwowarczyk <rad.piwowarczyk@gmail.com>
2021-01-05 10:48:14 +0100

    added addnotation in EmployeeServiceImpl

--- src/main/java/com/example/springbootthymeleafcrudwebapp/service/EmployeeServiceImpl.java
+++ src/main/java/com/example/springbootthymeleafcrudwebapp/service/EmployeeServiceImpl.java
@@ -3,11 +3,13 @@ package com.example.springbootthymeleafcrudwebapp.service;
 import com.example.springbootthymeleafcrudwebapp.model.Employee;
 import com.example.springbootthymeleafcrudwebapp.repository.EmployeeRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
 
 
 import java.util.List;
 
 @AllArgsConstructor
+@Service
 public class EmployeeServiceImpl implements EmployeeService {
 
     private EmployeeRepository employeeRepository;
