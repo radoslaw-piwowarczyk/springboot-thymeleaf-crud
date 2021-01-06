9e0985ab43c6f45c7c21889b61e57d4aaa0aef49
Radoslaw Piwowarczyk <rad.piwowarczyk@gmail.com>
2021-01-06 07:57:58 +0100

    added showFormUpdate method in controller

--- src/main/java/com/example/springbootthymeleafcrudwebapp/controller/EmployeeController.java
+++ src/main/java/com/example/springbootthymeleafcrudwebapp/controller/EmployeeController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -29,12 +30,21 @@ public class EmployeeController {
         model.addAttribute("employee", employee);
         return "new_employee";
     }
+
     @PostMapping("/saveEmployee")
-    public String saveEmployee(@ModelAttribute("employee") Employee employee){
+    public String saveEmployee(@ModelAttribute("employee") Employee employee) {
         //save employee to database
         employeeService.saveEmployee(employee);
         return "redirect:/";
     }
 
+    @GetMapping("/showFormUpdate/{id}")
+    public String showFormUpdate(@PathVariable(value = "id") long id, Model model) {
+        //get employee from the service
+        Employee employee = employeeService.getEmployeeById(id);
+        //set employee as a model attribute to pre-populate the form
+        model.addAttribute("employee", employee);
+        return "update_employee";
+    }
 
 }
